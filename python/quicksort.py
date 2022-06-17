5e73c6f5b8d43cee70e7a66fba48f7c64047cdee
Sdqumar <sdqumar09@gmail.com>
2022-06-16 21:05:49 -0700

    feat: quicksort

---
+++ python/quicksort.py
@@ -0,0 +1,21 @@
+def quicksort(values):
+    if len(values) <= 1:
+        return values
+    
+    less_than_pivot = []
+    greater_than_pivot = []
+    pivot = values[0]
+
+    for value in values[1:]:
+        if value <= pivot:
+            less_than_pivot.append(value)
+        else:
+            greater_than_pivot.append(value)
+
+    return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+
+
+numbers = [121,31,11,23,14,4,8,2,1,33,52,112]
+sorted_numbers = quicksort(numbers)
+
+print(sorted_numbers)
